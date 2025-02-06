216522b58f3a9a02466e476d69bc6c31f5a41920
OksanaVovk <95425405+OksanaVovk@users.noreply.github.com>
2025-02-06 21:46:48 +0000

    Deploying to gh-pages from @ OksanaVovk/cats_friends@96e66c498932ab84b4a66d725bea57c76777aa2d 🚀

---
+++ static/js/47.0d661a4b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[47],{9047:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(3161),c=n(184),o=function(){return(0,c.jsx)(r.Z,{sx:{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontSize:40,color:"#010101"},children:"404 NotFound"})},u=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(o,{})})}}}]);
+//# sourceMappingURL=47.0d661a4b.chunk.js.map
