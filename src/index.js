068d4ebbffa7816f8159e967d011d990e443c0d7
Vovk Oksana <oksvovk85@gmail.com>
2025-12-22 13:01:59 +0200

    changed path

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-        {/* <BrowserRouter basename="/cats_friends/"> */}
-        <BrowserRouter>
+        <BrowserRouter basename="/cats_friends/">
           <Scroll />
           <App />
         </BrowserRouter>
